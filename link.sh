0417a908445f3102b835bd2ff56dbf1798993634
jpxcz <jop0693@gmail.com>
2025-10-26 17:02:11 -0600

    waybar style

--- link.sh
+++ link.sh
@@ -4,6 +4,7 @@ ln -sf $(pwd)/zshrc ~/.zshrc
 ln -sf $(pwd)/hyprland.conf ~/.config/hypr/hyprland.conf
 ln -sf $(pwd)/hyprlock.conf ~/.config/hypr/hyprlock.conf
 ln -sf $(pwd)/waybar.conf ~/.config/waybar/config
+ln -sf $(pwd)/waybar.css ~/.config/waybar/style.css
 ln -sf $(pwd)/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf $(pwd)/tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/nvim ~/.config/nvim
