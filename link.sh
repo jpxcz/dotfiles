63cc9ccf63a072a46c0e69abf0070c7db67ae7e3
jpxcz <jop0693@gmail.com>
2025-10-26 13:49:38 -0600

    working config

--- link.sh
+++ link.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -sf ./zshrc ~/.zshrc
-ln -sf ./hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sf ./hyprlock.conf ~/.config/hypr/hyprlock.conf
-ln -sf ./waybar.conf ~/.config/waybar/config
-ln -sf ./kitty.conf ~/.config/kitty/kitty.conf
-ln -sf ./tmux.conf ~/.tmux.conf
-ln -sf ./nvim ~/.config/nvim
+ln -sf $(pwd)/zshrc ~/.zshrc
+ln -sf $(pwd)/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sf $(pwd)/hyprlock.conf ~/.config/hypr/hyprlock.conf
+ln -sf $(pwd)/waybar.conf ~/.config/waybar/config
+ln -sf $(pwd)/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf $(pwd)/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/nvim ~/.config/nvim
