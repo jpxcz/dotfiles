8676e277788ba420b45def48aa21ddd14c7593ce
jpxcz <jop0693@gmail.com>
2025-10-26 17:17:17 -0600

    hypridle and other dependencies

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@
 ln -sf $(pwd)/zshrc ~/.zshrc
 ln -sf $(pwd)/hyprland.conf ~/.config/hypr/hyprland.conf
 ln -sf $(pwd)/hyprlock.conf ~/.config/hypr/hyprlock.conf
+ln -sf $(pwd)/hypridle.conf ~/.config/hypr/hypridle.conf
 ln -sf $(pwd)/waybar.conf ~/.config/waybar/config
 ln -sf $(pwd)/waybar.css ~/.config/waybar/style.css
 ln -sf $(pwd)/kitty.conf ~/.config/kitty/kitty.conf
